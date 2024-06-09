7614d53ea2fd91ff3c207c60a88368177c98ef4f
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2024-06-09 15:08:59 -0300

    feat: separa contextos de testes do jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,40 @@
 import nextJest from "next/jest.js";
 import type { Config } from "jest";
+import { pathsToModuleNameMapper } from "ts-jest";
+
+import { compilerOptions } from "./tsconfig.json";
 
 const createJestConfig = nextJest({
   dir: `./`,
 });
 
-const config: Config = {
-  coverageProvider: `v8`,
-  testEnvironment: `jsdom`,
-  setupFilesAfterEnv: [`<rootDir>/jest.setup.ts`],
+const config = async () => {
+  const server = createJestConfig({
+    displayName: `server`,
+    testEnvironment: `node`,
+    preset: `ts-jest`,
+    testMatch: [`<rootDir>/src/server/**/?(*.)+(spec|test).[jt]s?(x)`],
+    setupFilesAfterEnv: [`<rootDir>/jest.setup.ts`],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+      prefix: `<rootDir>/`,
+    }),
+  });
+
+  const client = createJestConfig({
+    displayName: `client`,
+    testEnvironment: `jsdom`,
+    preset: `ts-jest`,
+    testMatch: [`<rootDir>/src/client/**/?(*.)+(spec|test).[jt]s?(x)`],
+    setupFilesAfterEnv: [`<rootDir>/jest.setup.ts`],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+      prefix: `<rootDir>/`,
+    }),
+  });
+
+  return {
+    coverageProvider: `v8`,
+    projects: [await server(), await client()],
+  } satisfies Config;
 };
 
-export default createJestConfig(config);
+export default config();
