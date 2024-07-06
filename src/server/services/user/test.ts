df335c163ec96e079ceb2bd4481eef5958e9f987
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2024-07-06 10:48:45 -0300

    test: testa criação de usuário no UserService

--- src/server/services/user/test.ts
+++ src/server/services/user/test.ts
@@ -9,11 +9,149 @@ import { ValidationError } from "@server/utils/validator/error";
 import { fakerPT_BR as faker } from "@faker-js/faker";
 
 import { UserService } from ".";
-import { type UserServiceChangePasswordParams } from "./types";
+import { type UserServiceChangePasswordParams, type UserServiceCreateParams } from "./types";
 
 describe(`UserService`, () => {
   afterEach(() => {
     jest.clearAllMocks();
+  });
+
+  describe(`create`, () => {
+    describe(`should throw an error if email is invalid`, () => {
+      const invalidEmails = [
+        `invalid-email`,
+        `invalid-email.com`,
+        `invalid-email@`,
+        `invalid-email@domain`,
+        `invalid-email@domain.`,
+        `invalid-email@.com`,
+        `@invalid-email.com`,
+      ];
+
+      invalidEmails.forEach(email =>
+        it(`when email is: ${email}`, async () => {
+          // Arrange
+          const params: UserServiceCreateParams = {
+            name: faker.person.fullName(),
+            email,
+            password: faker.internet.password(),
+          };
+
+          try {
+            // Act
+            await UserService.create(params);
+            fail(`should throw an error if email is invalid`);
+          } catch (error) {
+            // Assert
+            expect(error).toBeInstanceOf(ValidationError);
+            expect(error).toHaveProperty(`message`, `Email inválido`);
+          }
+        }),
+      );
+    });
+
+    it(`should throw an error if password have length < 8`, async () => {
+      // Arrange
+      const params: UserServiceCreateParams = {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        password: `1234567`,
+      };
+
+      try {
+        // Act
+        await UserService.create(params);
+        fail(`should throw an error if password have length < 8`);
+      } catch (error) {
+        // Assert
+        expect(error).toBeInstanceOf(ValidationError);
+        expect(error).toHaveProperty(`message`, `A senha deve conter no mínimo 8 caracteres`);
+      }
+    });
+
+    it(`should throw an error if password contains only numbers`, async () => {
+      // Arrange
+      const params: UserServiceCreateParams = {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        password: `12345678`,
+      };
+
+      try {
+        // Act
+        await UserService.create(params);
+        fail(`should throw an error if password contains only numbers`);
+      } catch (error) {
+        // Assert
+        expect(error).toBeInstanceOf(ValidationError);
+        expect(error).toHaveProperty(`message`, `A senha deve conter letras e números`);
+      }
+    });
+
+    it(`should throw an error if password contains only letters`, async () => {
+      // Arrange
+      const params: UserServiceCreateParams = {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        password: `abcdefgh`,
+      };
+
+      try {
+        // Act
+        await UserService.create(params);
+        fail(`should throw an error if password contains only letters`);
+      } catch (error) {
+        // Assert
+        expect(error).toBeInstanceOf(ValidationError);
+        expect(error).toHaveProperty(`message`, `A senha deve conter letras e números`);
+      }
+    });
+
+    it(`should throw an error if email is already registered`, async () => {
+      // Arrange
+      const user = new UserFactory();
+      const params: UserServiceCreateParams = {
+        name: faker.person.fullName(),
+        email: user.email,
+        password: faker.internet.password(),
+      };
+
+      // Mock
+      UserRepositoryMock.findByEmail.mockResolvedValue(user);
+
+      try {
+        // Act
+        await UserService.create(params);
+        fail(`should throw an error if email is already registered`);
+      } catch (error) {
+        // Assert
+        expect(error).toBeInstanceOf(ValidationError);
+        expect(error).toHaveProperty(`message`, `Email já cadastrado`);
+      }
+    });
+
+    it(`should create a new user with success`, async () => {
+      // Arrange
+      const params: UserServiceCreateParams = {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        password: `C4RLOS-LOUR3IR0`,
+      };
+
+      // Mock
+      UserRepositoryMock.findByEmail.mockResolvedValue(null);
+
+      // Act
+      const result = await UserService.create(params);
+
+      // Assert
+      expect(result).toBeDefined();
+      expect(UserRepositoryMock.create).toHaveBeenCalledWith({
+        name: params.name,
+        email: params.email,
+        password: CryptoJS.SHA256(params.password).toString(),
+      });
+    });
   });
 
   describe(`changePassword`, () => {
